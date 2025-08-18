e958ceb68413a470680976914799df30e7261200
mikaelyoa <mikaelyalmeida08@gmail.com>
2025-08-18 11:47:42 -0300

    Adicionar código das atividades práticas 01

---
+++ Atividade3_CalculadoraVolume/volume.py
@@ -0,0 +1,5 @@
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print("Volume:", volume, "cm³")
---
+++ Atividade5_CalculadoraDiferenca/diferenca.py
@@ -0,0 +1,7 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+diferenca = (A * B) - (C * D)
+print("DIFERENCA =", diferenca)
